58c8fc4f3e63ce1e638b488b222a87615c1cfa7b
Diwanshi Pandey <diwanshipandey331@gmail.com>
2025-09-20 22:56:21 +0530

    Update day5RemoveElementleetcode27.java

--- Easyproblems/day5RemoveElementleetcode27.java
+++ Easyproblems/day5RemoveElementleetcode27.java
@@ -1,7 +1,7 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         int ptr =0;
-        for (int i=0; i<nums.length-1; i++){
+        for (int i=0; i<nums.length; i++){
             if (nums[i]!= val){
                 nums[ptr] = nums[i];
                 ptr++;
